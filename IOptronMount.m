e6f69bd035e573d416c953a6e0bc1adcdbbe83e1
Enrico <enrico.segre@weizmann.ac.il>
2019-12-24 16:20:13 +0200

    implementing get position, status, homing

--- IOptronMount.m
+++ IOptronMount.m
@@ -3,9 +3,14 @@ classdef IOptronMount <handle
     properties % Position
         Az
         Alt
-        Status
+        Dec
+        RA
     end
         
+    properties(GetAccess=public, SetAccess=private)
+        Status
+    end
+
     properties(Hidden) % interrogable, but not of immediate use
         Port
         verbose=true;
@@ -71,8 +76,15 @@ classdef IOptronMount <handle
         end
         
         function resp=Query(I,cmd)
+            % Dispose of previous traffic potentially having
+            % filled the inbuffer, for an immediate response
+            flushinput(I.Port)
             fprintf(I.Port,':%s#',cmd);
-            pause(0.05);
+            if strcmp(cmd,'Q')
+                pause(0.5); % abort requires a longer delay
+            else
+                pause(0.1);
+            end
             resp=char(fread(I.Port,[1,I.Port.BytesAvailable],'char'));
             % possible replies are long strings terminated by #
             %  for get commands, or 0/1 for boolean gets, or setters
@@ -92,6 +104,71 @@ classdef IOptronMount <handle
         
         function delete(I)
             delete(I.Port)
+        end
+        
+    end
+    
+    methods % getter/setters: Position and status
+        
+        function S=get.Status(I)
+            % state enumerations - let the function error if an out-of
+            %  -range value is returned
+            gpsstate=["No GPS","no data","valid"];
+            motionstate=["stopped","track w/o PEC","slew","auto-guiding",...
+                         "meridian flipping","track with PEC","parked",...
+                         "at home"];
+            trackingrate=["sidereal","lunar","solar","King","custom"];
+            keyspeed=["1x","2x","4x","8x","16x","32x","64x","128x",...
+                      "256x","512x","max"];
+            timesource=["communicated","hand controller","GPS"];
+            hemisphere=["South","North"];
+            resp=I.Query('GLS');
+            S=struct('Lon',str2double(resp(1:9))/360000,...
+                     'Lat',str2double(resp(10:17))/360000-90,...
+                     'GPS',gpsstate(str2double(resp(18))+1),...
+                     'motion',motionstate(str2double(resp(19))+1),...
+                     'tracking',trackingrate(str2double(resp(20))+1),...
+                     'keyspeed',keyspeed(str2double(resp(21))+1),...
+                     'timesource',timesource(str2double(resp(22))+1),...
+                     'hempisphere',hemisphere(str2double(resp(23))+1) );
+        end
+
+        function AZ=get.Az(I)
+            resp=I.Query('GAC');
+            AZ=str2double(resp(1:9))/360000;
+        end
+        
+        function ALT=get.Alt(I)
+            resp=I.Query('GAC');
+            ALT=str2double(resp(10:17))/360000;
+        end
+        
+        function DEC=get.Dec(I)
+            resp=I.Query('GEP');
+            DEC=str2double(resp(1:9))/360000;
+        end
+        
+        function RA=get.RA(I)
+            resp=I.Query('GEP');
+            RA=str2double(resp(10:17))/360000;
+        end
+ 
+    end
+    
+    methods % Moving commands.
+        
+        function Abort(I)
+            % emergency stop
+            I.Query('Q')
+        end
+        
+        function GoHome(I)
+            I.Query('MH')
+        end
+        
+        function FullHoming(I)
+            I.Query('MSH')
+            % here, poll status and exit only when done
         end
         
     end
